705923844a99ffa0497640c382fe0c90917da2e0
MohamedElkhalfi <med09122021@outlook.fr>
2022-02-27 02:38:57 +0100

    Unit Test GetAllProducts

---
+++ Ecommerce.UnitTest/DAL/Repository/Products/ProductRepositoryUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.Core.Interfaces;
+using Ecommerce.Core.Services;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ecommerce.UnitTest.DAL.Repository.Products
+{
+    public class ProductRepositoryUnitTest
+    {
+        [Fact]
+        public void Should_Return_Data_And_Should_Not_Be_Null()
+        {
+            //Arrange
+            var _ProductRepositoryMock = new Mock<IProductRepository>();
+            var _ProductService = new ProductService(_ProductRepositoryMock.Object);
+
+            //Act
+           var GetAllProducts =   _ProductService.ViewAllProductsServiceAsync().Result.ToList();
+
+
+            //Assert 
+            GetAllProducts.Should().NotBeNull();
+            Assert.True(GetAllProducts.Count() >= 1);
+
+        }
+    }
+}
